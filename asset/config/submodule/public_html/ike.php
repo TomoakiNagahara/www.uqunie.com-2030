3f312beb078cb7a6d2c09e4191f7a8b57c5ded9d
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-02-27 01:09:08 +0900

    uqunie: asset > config > submodule > public_html > ike.php

---
+++ asset/config/submodule/public_html/ike.php
@@ -0,0 +1,26 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/public_html/ike.php
+ *
+ * @created    2026-02-27
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  0,
+	'path'   => 'ike',
+	'url'    => 'https://github.com/TomoakiNagahara/op-public-uqunie-com-ike.git',
+	'branch' => 'ike',
+];
