1dbbfb81f2dc260273967c4bcf192d084355f8b5
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-28 13:50:34 +0900

    uqunie: asset > config > cd.php | uqunie

---
+++ asset/config/cd.php
@@ -0,0 +1,21 @@
+<?php
+/**	op-unit-cd:/config.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-cd
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Namespace
+ *
+ */
+namespace OP\UNIT\CD;
+
+//	...
+return [
+	'branch' => [
+		'uqunie',
+	],
+];
